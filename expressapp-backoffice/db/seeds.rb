e62b1cb7a7c1d6556342a33bf361bb183b71013f
kenneth <kabt24@gmail.com>
2017-11-25 11:29:23 -0600

    sessions with devise gem

--- expressapp-backoffice/db/seeds.rb
+++ expressapp-backoffice/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(
+               email: 'admin@expresapp.com',
+               password: 'tkWcgY2ytwhWqWE9',
+               password_confirmation: 'tkWcgY2ytwhWqWE9'
+)
+user.save!
